b73aa57ae26a0fc6030100e29647dcd0ecf20d44
geta6 <getakura@gmail.com>
2014-12-13 20:30:23 +0900

    Update routing.spec.js

--- test/routing.spec.js
+++ test/routing.spec.js
@@ -6,7 +6,7 @@ var app = require(path.resolve('config', 'app'));
 var request = require('supertest').agent(app.listen());
 
 describe('Routing', function(){
-  it('should redirects', function(done){
+  it('should render index', function(done){
     request.get('/').expect(200).end(done);
   });
 });
